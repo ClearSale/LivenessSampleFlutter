c46137a8ff413ed603a5791b89f2ff394a8899c7
Victor <victor.oliveira@clear.sale>
2023-01-09 17:06:55 -0300

    Sample Flutter

---
+++ lib/liveness_store.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:cs_liveness_flutter/export.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/liveness_state.dart';
+
+class LivenessStore extends ValueNotifier<LivenessState> {
+  static const String clientId = "";
+  static const String clientSecret = "";
+
+  final _csLiveness = CsLiveness(
+    clientId: clientId,
+    clientSecret: clientSecret,
+  );
+  LivenessStore() : super(LivenessInitialState());
+
+  Future<void> start() async {
+    value = LivenessLoadingState(true);
+    try {
+      final result = await _csLiveness.start();
+      value = LivenessSuccessState(result);
+    } on CSLivenessCancelByUserException catch (e) {
+      value = LivenessErrorState("Usuário cancelou a ação.");
+    } catch (e) {
+      value = LivenessErrorState(e.toString());
+    }
+  }
+
+  void clean() {
+    value = LivenessInitialState();
+  }
+}
